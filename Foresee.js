a6bae18901ef4aba34e32abe764ee4204d5008f8
Tear <jequel07@gmail.com>
2021-01-19 22:21:44 -0500

    Adding starter files, including screenshot

---
+++ Foresee.js
@@ -0,0 +1,34 @@
+let userName = prompt('What is your name?');
+userName ? console.log(`Hello ${userName}`) : console.log(`Hello!`);
+
+let userQuestion = prompt("What is your question?");
+console.log(`The user asked: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
+switch(randomNumber) {
+  case 0:
+  eightball = "It is certain";
+  break;
+  case 1:
+  eightball = 'It is decidedly so';
+  break;
+  case 2:
+  eightball = 'Reply hazy try again';
+  break;
+  case 3:
+  eightball = 'Cannot predict now';
+  break;
+  case 4:
+  eightball = 'Do not count on it';
+  break;
+  case 5:
+  eightball = 'My sources say no';
+  break;
+  case 6:
+  eightball = 'Outlook not so good';
+  break;
+  case 7:
+  eightball = "Signs point to yes";
+  break;
+}
+console.log(eightball);
